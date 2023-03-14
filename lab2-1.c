2d2e084c38d8eb66ea9ba8a6a1222a8327911a07
bjaeyun <119185674+bjaeyun@users.noreply.github.com>
2023-03-14 16:06:31 +0900

    Create lab2-1.c

---
+++ lab2-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+char charType;
+int integerType;
+float floatType;
+double doubleType;
+printf("Size of char: %ld byte\n",sizeof(charType)); //변수 charType 자료형의 크기 출력
+printf("Size of int: %ld bytes\n",sizeof(integerType)); //변수 integerType 자료형의 크기 출력
+printf("Size of float: %ld bytes\n",sizeof(floatType)); //변수 floatType 자료형의 크기 출력
+printf("Size of double: %ld bytes\n",sizeof(doubleType)); // 변수 doubleType 자료형의 크기 출력
+printf("------------[변재윤] [2022041046]------------\n");
+printf("Size of char: %ld byte\n",sizeof(char)); //자료형 char의 크기 출력
+printf("Size of int: %ld bytes\n",sizeof(int)); //자료형 int의 크기 출력
+printf("Size of float: %ld bytes\n",sizeof(float)); //자료형 float의 크기 출력
+printf("Size of double: %ld bytes\n",sizeof(double)); //자료형 double의 크기 출력
+printf("------------[변재윤] [2022041046]------------\n");
+printf("Size of char*: %ld byte\n",sizeof(char*)); //자료형 char 포인터 변수의 크기 출력  
+printf("Size of int*: %ld bytes\n",sizeof(int*)); // 자료형 int 포인터 변수의 크기 출력
+printf("Size of float*: %ld bytes\n",sizeof(float*)); //자료형 float 포인터 변수의 크기 출력
+printf("Size of double*: %ld bytes\n",sizeof(double*)); //자료형 double 포인터 변수의 크기 출력
+return 0;
+}
